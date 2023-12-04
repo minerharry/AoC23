fa9a289c30c40f487e2417b9172702256b8126f7
minerharry <miner.harry567@gmail.com>
2023-12-03 23:46:34 -0500

    day3p2

--- day3-2.py
+++ day3-2.py
@@ -1,7 +1,9 @@
-# from imports import .s*
+from typing import DefaultDict
+from imports import *
 import text
 import re
-arr = text.get_input("input.txt")
+p = Puzzle(2023,3)
+arr = get_input(p)
 
 def issymbol(s:str):
     if len(s) != 1: raise Exception()
@@ -16,21 +18,21 @@ def get_neighbors(row:int,col:int):
         if o2 + col < 0:
             continue
         try:
-            yield arr[o1+row][o2+col]
+            yield (o1+row,o2+col)
         except:
             pass
 # parts = []
-partidx = []
+gears:dict[tuple[int,int],list[tuple[int,int]]] = DefaultDict(list)
 for r in range(len(arr)):
     for c,el in enumerate(arr[r]):
         if el == "*":
-            partidx += list(get_neighbors(r,c))
+            for g in (get_neighbors(r,c)):
+                gears[g].append((r,c))
         # if any([issymbol(v) for v in get_neighbors(r,c)]):
         #     # print(el)
         #     partidx.append((r,c))
             # parts.append(arr[r][c])
-# print(parts)
-print(partidx)
+print(gears)
 
 nums:list[tuple[tuple[int,int],int,int]] = []
 for r,row in enumerate(arr):
@@ -40,19 +42,23 @@ for r,row in enumerate(arr):
         if n.isdigit():
             nums.append(((r,idx),len(n),int(n)))
         idx += len(n) + 1
-# print(nums)
+print(nums)
 
-parts = []
+partdict:dict[tuple[int,int],list[int]] = DefaultDict(list)
 for n in nums:
     for k in range(n[0][1],n[0][1]+n[1]):
         # print(n)
         # print(k)
-        if (n[0][0],k) in partidx:
-            parts.append(n[2])
+        t = n[0][0],k
+        if t in gears:
+            for g in gears[t]:
+                partdict[g].append(n[2])
             break
-
-print(parts)
-print(sum(parts))
+print(partdict)
+# print(parts)
+# print(sum(parts))
+doubles = [v[0]*v[1] for _,v in partdict.items() if len(v) == 2]
+print(doubles)
+print(sum(doubles))
         
-
-print(list(get_neighbors(0,2)))
+submit_answer(sum(doubles),part='b',puzzle=p)
